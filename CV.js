54a199762f819115c0ee31b18d01f163b4f84ed2
L-Ismail <metalmachine1975@gmail.com>
2025-02-03 23:50:40 +0100

    Changement-2
    Ajouts

--- CV.js
+++ CV.js
@@ -1,3 +1,49 @@
-let f = document.getElementById("nbcol");  
 
-f.addEventListener("click", maj_nb_col); 
+class Moi {
+    nom = ""; // propriété nom
+    age = ""; // propriété age, par défaut 0
+    adresse="";
+    courriel=""
+    telephone=""
+    universite=""
+    permis=""
+    // Constructeur
+    constructor(nom, age, adresse, courriel, telephone, universite, permis) {
+        this.nom = nom;
+        this.age = age;
+        this.adresse=adresse;
+        this.courriel=courriel;
+        this.telephone=telephone;
+        this.universite=universite;
+        this.permis=permis;
+    }
+}
+
+function afficherInfos() {
+    var html="<div class='nom'><h1>"+this.nom+"</span></h1>";
+        html+="<div class='age'>"+this.age+"</div>";
+        html+="<div class='adresse'>"+this.adresse+"</div>";
+        // html+="<div class='image'><img src='media/"+this.image+"' /></div>";
+        html+="<div class='courriel'>"+this.courriel+"</div>";
+        html+="<div class= 'telephone'> "+this.telephone+"</div>";
+        html+="<div class= 'universite'> "+this.universite+"</div>";
+        html+="<div class='permis'>"+this.permis+"</div>";
+        return html;
+    }
+    var infos_perso = new Moi; 
+    infos_perso={
+        nom: "Ismaël Diallo",
+        age: "24 ans" ,
+        adresse: "28 Avenue du Giffre, Annemasse, Haute-Savoie, 74100",
+        courriel: "28avenuearistide@gmail.com",
+        telephone: "06 72 37 18 06",
+        universite: "IAE Savoie Mont-Blanc, Annecy-Le-Vieux",
+        permis: "Permis B",
+        display:afficherInfos
+    };
+    console.log(infos_perso)
+
+document.getElementById("infos").innerHTML=infos_perso.display()
+
+
+
---
+++ Competences.js
@@ -0,0 +1,24 @@
+
+src="https://cdn.jsdelivr.net/npm/chart.js"
+
+const xValues = ["C/C++", "Python", "Javascript/HTML/CSS", "SQL", "C#"];
+const yValues = [100, 95, 60, 98, 30];
+const barColors = ["red", "green","blue","orange","brown"];
+
+new Chart("graph", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Compétences logiciels en %"
+    }
+  }
+});
